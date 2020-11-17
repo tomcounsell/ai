c31731ee3879f84373520c0311a0d32c99fcbc96
Tom Counsell <other@tomcounsell.com>
2020-11-17 14:09:31 +0700

    run text inputs through rasa

--- apps/common/utilities/multithreading.py
+++ apps/common/utilities/multithreading.py
@@ -13,7 +13,7 @@ def start_new_thread(function):
         ```
     """
     def decorator(*args, **kwargs):
-        t = Thread(target = function, args=args, kwargs=kwargs)
+        t = Thread(target=function, args=args, kwargs=kwargs)
         t.daemon = True
         t.start()
     return decorator
--- apps/communication/models/telegram_bot_membership.py
+++ apps/communication/models/telegram_bot_membership.py
@@ -1,18 +1,22 @@
+import rasa
 import telegram
 import logging
 from django.contrib.postgres.fields import JSONField
 from django.db import models
+from rasa.core.agent import Agent as RasaAgent
+from rasa.core.channels import CollectingOutputChannel, UserMessage
+from rasa.core.channels.telegram import TelegramOutput, TelegramInput
 from telegram import Message, Update
 from telegram.ext import CallbackContext
-
+import asyncio
 from apps.common.behaviors import Timestampable
 from apps.common.models import Upload
-from settings import AUTH_USER_MODEL
+from settings import AUTH_USER_MODEL, TELEGRAM_BOT_API_TOKEN, DEBUG, BASE_DIR
 
 
 class TelegramBotMembership(Timestampable, models.Model):
-
-    user = models.ForeignKey(AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL, related_name="telegram_bot_memberships")
+    user = models.ForeignKey(AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL,
+                             related_name="telegram_bot_memberships")
     # all memberships are for the same bot atm
     # bot_username = models.CharField(max_length=31, null=True, blank=True, help_text="eg. MyShopBot")
 
@@ -28,7 +32,6 @@ class TelegramBotMembership(Timestampable, models.Model):
     @property
     def telegram_bot(self):
         return self.telegram_user.bot
-
 
     # MODEL FUNCTIONS
     def respond_to(self, update: Update, context: CallbackContext):
@@ -61,8 +64,28 @@ class TelegramBotMembership(Timestampable, models.Model):
 
             return f"saved as upload {upload.id}"
 
-
         if update.message.text:
-            return "🆗👍"
+            rasa = RasaAgent.load(f"{BASE_DIR}/rasa/models/20201113-154024.tar.gz")
+
+            # rasa_message = UserMessage(
+            #     text=update.message.text,
+            #     output_channel=TelegramOutput(access_token=TELEGRAM_BOT_API_TOKEN),
+            #     sender_id=str(self.telegram_user_id),
+            #     # input_channel=TelegramInput(access_token=TELEGRAM_BOT_API_TOKEN, debug_mode=DEBUG),
+            #     metadata=self.__dict__,
+            # )
+            # return rasa.handle_message(rasa_message)
+
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            result = loop.run_until_complete(
+                rasa.handle_text(
+                    text_message=update.message.text,
+                    output_channel=TelegramOutput(access_token=TELEGRAM_BOT_API_TOKEN),
+                    sender_id=str(self.telegram_user_id),
+                )
+            )
+            return
+            # return "🆗👍"
 
         return "start with a commmand. \n see /help"
