c26f8b365dfe0dda186f37a4d6cf658ae46c8d32
Tom Counsell <contact@tomcounsell.com>
2021-07-29 14:24:00 +0700

    much improved agent storage in redis

--- systems/structures/redis_storage/key_value.py
+++ systems/structures/redis_storage/key_value.py
@@ -1,10 +1,13 @@
+import ast
 import logging
 from settings.redis_db import redis_db
 from abc import ABC
+import msgpack
 
 from systems.structures.redis_storage import RedisStorageException, MuchException
 
 logger = logging.getLogger(__name__)
+ENCODING = 'utf-8'
 
 
 class KeyValueException(RedisStorageException):
@@ -19,18 +22,80 @@ class KeyValueStorage(ABC):
     prefixes are for general parent classes objects (eg. mammal:human:woman:Lisa )
     suffixes are for specific attributes (eg. Lisa:eye_color, Lisa:age, etc)
     """
+    _db_key: str = ""  # default will be self.__class__.__name__
+    _db_value: bytes = None
+    _value: bytes = None
+
+    # _value_type = None
 
     def __init__(self, *args, **kwargs):
         # key for redis storage
-        self.db_key_prefix = kwargs.get('key_prefix', "")
-        self.db_key = kwargs.get('key', self.__class__.__name__)
-        self.db_key_suffix = kwargs.get('key_suffix', "")
-
-        self.value = kwargs.get('value', "")
+        self._key_prefix = kwargs.get('key_prefix', "")
+        self._key = kwargs.get('key', self.__class__.__name__)
+        self._key_suffix = kwargs.get('key_suffix', "")
         self.force_save = kwargs.get('force_save', False)
+        if 'value' in kwargs:
+            self.value = kwargs.get('value')
 
     def __str__(self):
         return str(self.get_db_key())
+
+    def get_db_key(self, refresh=False):
+        if refresh or not self._db_key:
+            self._db_key = self.compile_db_key(
+                key_prefix=self._key_prefix,
+                key=self._key,
+                key_suffix=self._key_suffix
+            )
+            # todo: add this line for using env in key
+            # + "" if SIMULATED_ENV == "PRODUCTION" else str(SIMULATED_ENV)
+        return self._db_key
+
+    @property
+    def value(self):
+        if self._value is None:
+            if self._db_value is None:
+                self._db_value = self._get_db_value(self.get_db_key())
+                self._value = self._db_value  # may stay None
+        if self._value is not None:
+            return msgpack.loads(self._value)
+        return None
+
+    @value.setter
+    def value(self, new_value):
+        # self._value_type = type(new_value)
+        self._value = msgpack.dumps(new_value)
+
+    def save(self, pipeline=None, *args, **kwargs):
+        if self._value is None:
+            raise KeyValueException("no value set, use delete() to permanently remove")
+        if not self.force_save:
+            # validate some rules here?
+            pass
+        # logger.debug(f'savingkey, value: {self.get_db_key()}, {self.value}')
+
+        if pipeline is not None:
+            pipeline = pipeline.set(self.get_db_key(), self._value)
+            self._db_value = None  # becomes unknown
+            return pipeline
+        else:
+            db_response = redis_db.set(self.get_db_key(), self._value)
+            if db_response is True:
+                self._db_value = self._value
+            return db_response
+
+    def delete(self, pipeline=None, *args, **kwargs):
+        if pipeline is not None:
+            pipeline = pipeline.delete(self.get_db_key())
+            return pipeline
+        else:
+            db_response = redis_db.delete(self.get_db_key())
+            if db_response >= 0:
+                return True
+
+    def revert(self):
+        self._db_value = self._get_db_value(self.get_db_key(refresh=True))
+        self._value = self._db_value
 
     @classmethod
     def compile_db_key(cls, key: str, key_prefix: str, key_suffix: str) -> str:
@@ -41,27 +106,7 @@ class KeyValueStorage(ABC):
             f':{key_suffix.strip(":")}'
         ).replace("::", ":").strip(":")
 
-    def get_db_key(self):
+    @classmethod
+    def _get_db_value(cls, db_key: str = "", *args, **kwargs):
+        return redis_db.get(db_key) if db_key else None  # also returns None if key not found
 
-        # default for self.db_key is already self.__class__.__name__
-        return str(
-            self.compile_db_key(
-                key_prefix=self.db_key_prefix,
-                key=self.db_key,
-                key_suffix=self.db_key_suffix
-            )
-            # todo: add this line for using env in key
-            # + f':{SIMULATED_ENV if SIMULATED_ENV != "PRODUCTION" else ""}'
-        )
-
-    def save(self, pipeline=None, *args, **kwargs):
-        if not self.value:
-            raise KeyValueException("no value set, nothing to save!")
-        if not self.force_save:
-            # validate some rules here?
-            pass
-        # logger.debug(f'savingkey, value: {self.get_db_key()}, {self.value}')
-        return redis_db.set(self.get_db_key(), self.value)
-
-    def get_value(self, db_key="", *args, **kwargs):
-        return redis_db.get(db_key or self.get_db_key())
