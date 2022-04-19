ce2d1a75beb5702f615347712c1a398ca3274580
Tom Counsell <contact@tomcounsell.com>
2022-04-19 21:38:54 +0700

    brain wave cycle

--- systems/theory/flection.py
+++ systems/theory/flection.py
@@ -23,6 +23,16 @@ class Flection(Model):
     strength = IntField(default=0, max_value=15)
     # _angle? = IntField(default=91, max_value=180)  # odd number [1, .., 179]
 
-    def grow_stronger(self, pipeline: Pipeline, amount=1) -> Pipeline:
-        pipeline.hincrby(self.db_key.redis_key, "strength", amount)
-        return pipeline
+    def strengthen(self, pipeline: Pipeline, amount=1) -> Pipeline:
+        if self.strength == 15:
+            return pipeline
+        elif self.strength + amount > 15:
+            amount = 15 - self.strength
+        return pipeline.hincrby(self.db_key.redis_key, "strength", amount)
+
+    def weaken(self, pipeline: Pipeline, amount=1) -> Pipeline:
+        if self.strength == 0:
+            return pipeline
+        elif self.strength - amount < 0:
+            amount = self.strength
+        return pipeline.hincrby(self.db_key.redis_key, "strength", amount)
