8849d52bb900ae869c5131bde005e54c2d38aaaf
Tom Counsell <other@tomcounsell.com>
2022-06-22 19:34:21 +0700

    pseudo utility

--- systems/agent/whiskey/agent.py
+++ systems/agent/whiskey/agent.py
@@ -7,3 +7,16 @@ class Agent(Model):
     output_SDRs = DictField()
     flections = ListField()
     utility_score = SortedField()
+
+    # grammar - capable complexity of communication
+    # vocabulary - communication limits nouns, verbs, adjectives
+
+
+
+
+def calc_value_score():
+
+    utility = agent.utility_score  # the value as voted by other agents
+    cost = agent.cost  # the cost of operation = the count of flections
+    # maybe need to normalize first
+    return utility - cost
