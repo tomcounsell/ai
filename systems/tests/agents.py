dd446f8427b604fae344b202c9818efec0c3aeb0
Tom Counsell <other@tomcounsell.com>
2022-08-11 14:01:04 +0700

    formating

--- systems/tests/agents.py
+++ systems/tests/agents.py
@@ -1,46 +1,58 @@
 import sys
+from unittest import TestCase
 
-from django.test import TestCase
-
-from systems.agent.agent import Agent
+from systems.agent.foxtrot.agent import Agent
 from systems.stimulus.vision import Vision
 
 lisa_info_dict = {
-    'generic': "mammal:human:woman",
-    'group': "BlackPink",
-    'friends': ["Jisoo", "Rose", "Jenny", ],
-    'age': 24,
-    'specifics': {'favorite:color': "yellow", },
+    "generic": "mammal:human:woman",
+    "group": "BlackPink",
+    "friends": [
+        "Jisoo",
+        "Rose",
+        "Jenny",
+    ],
+    "age": 24,
+    "specifics": {
+        "favorite:color": "yellow",
+    },
 }
 
 rose_info_dict = {
-    'generic': "mammal:human:woman",
-    'group': "BlackPink",
-    'friends': ["Jisoo", "Lisa", "Jenny", ],
-    'birthday_string': "February 11, 1997",
-    'specifics': {'favorite:color': "baby pink", },
+    "generic": "mammal:human:woman",
+    "group": "BlackPink",
+    "friends": [
+        "Jisoo",
+        "Lisa",
+        "Jenny",
+    ],
+    "birthday_string": "February 11, 1997",
+    "specifics": {
+        "favorite:color": "baby pink",
+    },
 }
 
 
 class test_agent(TestCase):
-
     def setup(self):
-        self.assertEqual('utf-8', sys.getdefaultencoding())
+        self.assertEqual("utf-8", sys.getdefaultencoding())
 
     def test_can_create_agent(self):
         self.lisa = Agent(name="Lisa")
-        self.lisa.stimuli.append({
-            'class': Vision,
-            'static_params': {
-                'zoom': 0,
-                'noise_strength': 0.12345,
-                'compression_seed': 12461,
-            },
-            'motor_params': {
-                'distance_from_center': 0,
-                'angle_from_center': 0,
-            },
-        })
+        self.lisa.stimuli.append(
+            {
+                "class": Vision,
+                "static_params": {
+                    "zoom": 0,
+                    "noise_strength": 0.12345,
+                    "compression_seed": 12461,
+                },
+                "motor_params": {
+                    "distance_from_center": 0,
+                    "angle_from_center": 0,
+                },
+            }
+        )
         self.lisa.save()
 
         # self.rose = Agent("Rose")
@@ -48,9 +60,10 @@ class test_agent(TestCase):
     def test_can_stimulate(self):
         self.lisa = Agent("Lisa")
         from systems.data.camera import Camera
+
         with Camera() as webcam:
             vision = Vision(webcam)
-            self.lisa.stimulate(Vision, data={'image': vision.image})
+            self.lisa.stimulate(Vision, data={"image": vision.image})
 
     def test_can_predict(self):
         pass
