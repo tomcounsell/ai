0b0f11c931ef3f8a0e65f63bb00a0fd042d26409
Tom Counsell <other@tomcounsell.com>
2023-03-27 17:02:25 +0700

    server config

--- config/database.py
+++ config/database.py
@@ -7,15 +7,16 @@ from systems.agent.steve.documents.users import User
 
 
 async def startup_database(app):
+    from systems.agent.steve.documents import active_document_models
+
     app.client = (
         AsyncIOMotorClient() if LOCAL else AsyncIOMotorClient(settings.mongodb_url)
     )
     app.database = app.client[settings.mongodb_db_name]
-    document_models = [
-        User,
-    ]
-
-    await init_beanie(database=app.database, document_models=document_models)
+    await init_beanie(
+        database=app.database,
+        document_models=active_document_models,
+    )
 
 
 def shutdown_database(app):
--- config/server.py
+++ config/server.py
@@ -1,9 +1,18 @@
+import dotenv
 from fastapi import FastAPI
 from mangum import Mangum
 from api.v1.router import router as api_router
 
 from config.database import shutdown_database, startup_database
+from utilities.n3d1117_chatgpt_telegram_bot_bot.main import (
+    main as chat_gpt_telegram_bot,
+)
 
+# Telegram Bot
+dotenv.load_dotenv("config/.env")
+chat_gpt_telegram_bot()
+
+# FastAPI Server
 app = FastAPI()
 
 
@@ -16,7 +25,7 @@ async def startup_event():
 @app.on_event("shutdown")
 async def shutdown_event():
     print("app is shutting down")
-    shutdown_database(app)
+    await shutdown_database(app)
 
 
 @app.get("/")
