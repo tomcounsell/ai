3a1dd5788bde003423e3562919c5838c801c2ea8
Tom Counsell <other@tomcounsell.com>
2023-04-30 10:30:16 +0700

    load new api keys, try a script

--- config/settings.py
+++ config/settings.py
@@ -7,9 +7,20 @@ class Settings(BaseSettings):
     mongodb_url: str = None
     mongodb_db_name: str = "ai"
 
+    # Chat Interfaces
+    telegram_bot_token: str = None
+    slack_token: str = None
+
     # GITHUB
     github_username: str = None
     github_access_token: str = None
+
+    # 3rd party API's
+    openai_api_key: str = None
+    serpapi_api_key: str = None
+    wolfram_alpha_appid: str = None
+    extractor_api_key: str = None
+    superface_sdk_token: str = None
 
     class Config:
         env_file = "config/.env"
