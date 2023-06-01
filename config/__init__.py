37c68dfc60563ccb1fa391ca1fed1ac5ed78f5b2
Tom Counsell <other@tomcounsell.com>
2023-06-01 20:08:09 +0700

    drop api app, fastapi, and beanie models

--- config/__init__.py
+++
@@ -1,1 +0,0 @@
-from config.settings import PRODUCTION, STAGE, LOCAL, DEBUG
--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.agent import Agent, Title
-from models.conversation import Role, Conversation
-
-active_db_models = [
-    Agent,
-    Conversation,
-]
