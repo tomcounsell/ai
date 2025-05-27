61067118916809def34c8eedff9b25a306e90c90
valor4labor <valor@yuda.me>
2025-05-27 10:40:29 +0700

    move to archive

--- _archive_/apps/common/tests/test_models/test_address.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-from ..test_behaviors import TimestampableTest
-from ...models import Address
-
-
-class AddressTest(TimestampableTest, TestCase):
-    model = Address
--- _archive_/apps/communication/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .email import Email
-from .sms import SMS
-from .telegram_bot_membership import TelegramBotMembership
-
-
-__all__ = ['Email', 'SMS', 'TelegramBotMembership',]
